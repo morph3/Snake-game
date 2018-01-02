e06b8733e28f04c91ee8e12de1517b928a5f2004
morph3 <melihkaanyildiz@gmail.com>
2018-01-03 01:57:48 +0300

    Update snake_game.c
    Changed all structure of the game . Still in complete

--- snake_game.c
+++ snake_game.c
@@ -1,209 +1,195 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include <conio.h>
-#define FALSE 0
-#define TRUE 1
-const int SIZE=20;
-enum direction{
-	STOP=0,
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT,
+#include <list>
+#include <windows.h>
+#include <time.h>
+#include <string>
+using namespace::std;
+
+
+struct sSnakeStruct{
+    int x;
+    int y;
 };
-enum direction dir;
-void draw(int *gameOver, int *score ,int *tailLength,int snake[], int fruitArray[], int tailX[], int tailY[]);
-int score_board();
-void generate_fruit(int SIZE,int fruit[]);
+
+enum eDirection{
+    STOP = 0,
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+}dir;
+
 void input();
-void play(int snake[]);
-void tail(int snakeX , int snakeY, int tailLength , int tailX[] , int tailY[]);
-void display(int array[], int length);
-// TODO : add a menu function that configures the game .
-// TODO : add a scoreboard .
-
+void play(list<sSnakeStruct> &snake);
 int main(){
-	// TODO : change variable and function names .
-	int fruitArray[2];
-	int snake[2]={5,5}; // initializing snake
-	int tailX[100], tailY[100];
-	tailX[0] = snake[0];// tail initilazing 
-	tailY[0] = snake[1];// tail initilazing 
-	
 
-	int tailLength = 1;
-	int score=0;
-	int gameOver = FALSE;
-	srand(time(NULL));
-	generate_fruit(SIZE,fruitArray);
-	
-	while(!gameOver){
-		fflush(stdin);
-		draw(&gameOver ,&score ,&tailLength,snake,fruitArray,tailX , tailY);
-		input();
-		play(snake);
-		printf("Score : %d ", score);
-		Sleep(70);
-	}
-	printf("Game Over");
-	return 0;
+    srand(time(NULL));
+
+    list<sSnakeStruct> snake = {{4,5},{5,5},{6,5},{7,5},{8,5},{9,5}};
+    const int nScreenWidth = 120;
+    const int nScreenHeight = 25;
+    int nFruitX = 1;
+    int nFruitY = 10;
+
+    int nScore = 0;
+
+    bool isDead = false;
+    // snake is heading towards
+    dir = WEST;
+
+    char *screen = new char[nScreenWidth*nScreenHeight];
+    WORD *screenAttr = new WORD[nScreenWidth*nScreenHeight];
+    for(int j = 0 ; j< nScreenWidth*nScreenHeight ; j++){
+        screenAttr[j] = FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED;
+    }
+    for (int i = 0; i < nScreenWidth*nScreenHeight; i++) {
+        screen[i] = ' ';
+    }
+
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+    DWORD dwAttrBytesWritten = 0;
+
+
+    string Title = "Snake Game  ---- Score :  ";
+    string Score;
+
+    unsigned int TIME_SLEEP = 100;
+
+
+    while(!isDead){
+        Score = to_string(nScore);
+
+        SetConsoleTitle((Title+Score).c_str());
+
+
+        //printf(&screen[1 + nScreenWidth],"Score : %d" , nScore );
+
+
+        Sleep(TIME_SLEEP);
+        //emptying the pixels each time
+        for (int i = 0; i < nScreenWidth*nScreenHeight; i++){
+            screen[i] = ' ';
+            screenAttr[i] = FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED;
+        }
+
+        // draw border
+        for (int i = 0; i < nScreenWidth; i++) {
+            screen[i] = '=';
+            screen[(nScreenHeight-1) * nScreenWidth +i] = '=';
+
+            screenAttr[i] = FOREGROUND_RED;
+            screenAttr[(nScreenHeight-1) * nScreenWidth +i] = FOREGROUND_RED;
+
+        }
+        //snake body drawing
+        int j = 0;
+        for (auto item : snake) {
+            if(isDead){
+                screen[item.y * nScreenWidth + item.x] = '+';
+            }
+            else{
+                // snakes head
+                if(j == 0 ){
+                    screen[item.y * nScreenWidth + item.x] = 'O';
+                    screenAttr[item.y * nScreenWidth + item.x ] = FOREGROUND_GREEN;
+                    j++;
+                    continue;
+                }
+                screenAttr[item.y * nScreenWidth + item.x ] = FOREGROUND_GREEN;
+                screen[item.y * nScreenWidth + item.x] = 'o';
+            }
+
+        }
+
+        // user input
+        input();
+
+        //snake movement
+        play(snake);
+
+        //fruit display
+        screen[(nFruitY ) * nScreenWidth  + (nFruitX)] = 'F';
+        screenAttr[(nFruitY ) * nScreenWidth  + (nFruitX)] = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+
+        //fruit snake collision
+        if(snake.front().x == nFruitX && snake.front().y == nFruitY){
+            // each fruit makes snakes grow 5 box
+            for(int i = 0 ; i < 5 ; i++){
+                snake.push_back({nFruitX,nFruitY});
+            }
+            nFruitX = rand()%nScreenWidth;
+            nFruitY = (rand() % (nScreenHeight-3))+2;
+            nScore ++;
+            if(TIME_SLEEP < 70){
+                TIME_SLEEP = TIME_SLEEP - 1;
+            }
+            else{
+                TIME_SLEEP = TIME_SLEEP - 2;
+            }
+        }
+
+        // snake wall collision
+
+        // cover the length of snake
+        snake.pop_back();
+
+        WriteConsoleOutputAttribute(hConsole, screenAttr, nScreenWidth*nScreenHeight , {0,0}, &dwAttrBytesWritten);
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+    }
+
+    system("pause");
+    return 0;
 }
-
-void draw(int *gameOver, int *score ,int *tailLength,int snake[], int fruitArray[], int tailX[], int tailY[]){
-	system("cls");
-	int snakeX, snakeY;
-	snakeX= snake[0];
-	snakeY= snake[1];	
-	
-	// refreshing tail everytime
-	tail(snakeX , snakeY ,*tailLength,tailX,tailY);
-	int x,y; // coordinates of table
-	for(x=0 ; x<=SIZE ; x++){
-		for(y=0 ; y<=SIZE; y++){
-			
-			//snake head drawing
-			if( x == snakeX && y == snakeY ){
-				printf("S");
-				continue;
-			}
-			// fruit drawing
-			if(x==fruitArray[0] && y==fruitArray[1]){
-				printf("X");
-				continue;
-			}
-			// if snake eats the fruit
-			if(snakeX == fruitArray[0] && snakeY == fruitArray[1]){
-				generate_fruit(SIZE , fruitArray);
-				(*score)++ ;
-				(*tailLength)++;
-			}
-			
-			// snake hits the wall
-			if(snakeX == 0 || snakeY ==0 || snakeX == SIZE || snakeY == SIZE){
-				*gameOver = TRUE;
-			}
-			if(x==0 || y==0 || y==SIZE || x==SIZE){
-				printf("#");
-			}
-
-			else{
-				int j = 0;
-				int printed = 0;
-			// tail printing 
-				for(j ; j < *(tailLength); j++ ){
-					if(x == tailX[j] && y == tailY[j]){
-						printf("s");
-						printed = 1;
-					}
-				}
-			//to avoid excess blanks
-			if(!printed){
-				printf(" ");	
-			}
-				
-			}	
-		}
-		printf("\n");
-	}
-}
-
-
-void generate_fruit(int SIZE, int fruit[]){
-	// TODO : make sure that fruit does not appear under snake .
-	//fruit[0] == fruitX
-	//fruit[1] == fruitY
-	fruit[0] = rand()% SIZE;
-	fruit[1] = rand()% SIZE;
-	if(fruit[0]== 0){
-		fruit[0]++;
-	}
-	if(fruit[1] == 0){
-		fruit[1]++;
-	}
-
-}
-
 
 void input(){
-	fflush(stdin);
-	//kbhit in c
-	//kbhit in c: kbhit function is used to determine if a key has been pressed or not. 
-	//To use kbhit function in your program you should include the header file "conio.h". 
-	//If a key has been pressed then it returns a non zero value otherwise returns zero.
-	if(_kbhit()){
-		switch(_getch()){
-			case 'w':
-				dir = UP;
-				break;
-			case 'a':	
-				dir = LEFT;
-				break;
-			case 'd':	
-				dir = RIGHT;
-				break;	
-			case 's':
-				dir = DOWN; 
-				break;
-			case 'x':
-				dir = STOP;
-			default:
-				break;
-		}
-	}	
-	return;
+    fflush(stdin);
+    if(_kbhit()){
+        switch(_getch()){
+            case 'W':
+            case 'w':
+                dir = NORTH;
+                break;
+            case 'D':
+            case 'd':
+                dir = EAST;
+                break;
+            case 'A':
+            case 'a':
+                dir = WEST;
+                break;
+            case 'S':
+            case 's':
+                dir = SOUTH;
+                break;
+            case 'x':
+                dir = STOP;
+
+            default:
+                break;
+        }
+    }
 }
 
-void play(int snake[]){
-	/*int snakeX, snakeY;
-	snakeX = snake[0];
-	snakeY = snake[1];*/
-	switch(dir){
-		case UP:
-			snake[0]--;
-			break;
-		case DOWN:
-			snake[0]++;
-			break;
-		case LEFT:
-			snake[1]--;
-			break;
-		case RIGHT:
-			snake[1]++;
-			break;
-		case STOP:
-			break;
-		default:
-			break;
-	}
-	printf("Snake coordiante : %d x %d y \n", snake[0], snake[1]);
-	return;
-}
-
-void tail(int snakeX , int snakeY, int tailLength , int tailX[] , int tailY[]){
-	int i,j;
-	int tempArrayX[(tailLength)];
-	int tempArrayY[(tailLength)];
-
-	// refreshing tail 
-	// basically tail has previous snake coordinates everytime 
-	// tailX part
-	for(i =0; i <(tailLength) ; i++){
-		tempArrayX[i] = tailX[i];
-	}
-	tailX[0] = snakeX;
-	for(j = 1; j <tailLength ; j++){
-		tailX[j] = tempArrayX[j-1];
-	}
-
-	////////////////////////////////////////////////////////////////////
-	// tailY part
-	for(i =0; i <(tailLength) ; i++){
-		tempArrayY[i] = tailY[i];
-	}
-	tailY[0] = snakeY;
-	for(j = 1; j <(tailLength) ; j++){
-		tailY[j] = tempArrayY[j-1];
-	}		
-	
-	
+void play(list<sSnakeStruct> &snake){
+    switch (dir) {
+        case NORTH:
+            snake.push_front({snake.front().x, snake.front().y-1});
+            break;
+        case EAST:
+            snake.push_front({snake.front().x+1, snake.front().y});
+            break;
+        case SOUTH:
+            snake.push_front({snake.front().x,snake.front().y+1});
+            break;
+        case WEST:
+            snake.push_front({snake.front().x-1,snake.front().y});
+            break;
+        case STOP:
+            break;
+        default:
+            break;
+    }
 }
