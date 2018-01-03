3e069222d69871564d759f17daac42cc9f16997c
morph3 <melihkaanyildiz@gmail.com>
2018-01-03 18:22:23 +0300

    Update snake_game.cpp

--- snake_game.cpp
+++ snake_game.cpp
@@ -26,16 +26,20 @@ void play(list<sSnakeStruct> &snake);
 int main(){
     srand(time(NULL));
     list<sSnakeStruct> snake = {{17,5},{16,5},{15,5},{14,5},{13,5},{12,5}};
-    int nScreenWidth = 40;
-    int nScreenHeight = 25;
+    int nScreenWidth = 50;
+    int nScreenHeight = 40;
     int nFruitX = rand()%nScreenWidth;
     int nFruitY = (rand() % (nScreenHeight-3))+2;
     float fScore = 1.0f;
     float fScoreConstant = 5.0f;
     bool isDead = false;
+    unsigned int TIME_SLEEP = 100;
+    char cScore[50]="";
+    string sTitle = "Snake game ----- Score : ";
 
     // snake is heading towards
     dir = EAST;
+
 
     char *screen = new char[nScreenWidth*nScreenHeight];
     WORD *screenAttr = new WORD[nScreenWidth*nScreenHeight];
@@ -46,30 +50,44 @@ int main(){
         screen[i] = ' ';
     }
 
-    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-    SetConsoleActiveScreenBuffer(hConsole);
-	
-	DWORD dwBytesWritten = 0;
+	// Thanks to Javidx9, OneLoneCoder 
+
+		//"It seems that the console behaves differently on some systems
+		// and I'm unsure why this is. It could be to do with windows default settings, or
+		// screen resolutions, or system languages. Unfortunately, MSDN does not offer much
+		// by way of useful information, and so the resulting sequence is the reult of experiment
+		// that seems to work in multiple cases.
+		//
+		// The problem seems to be that the SetConsoleXXX functions are somewhat circular and 
+		// fail depending on the state of the current console properties, i.e. you can't set
+		// the buffer size until you set the screen size, but you can't change the screen size
+		// until the buffer size is correct. This coupled with a precise ordering of calls
+		// makes this procedure seem a little mystical :-P. Thanks to wowLinh for helping - Jx9"
+
+    DWORD dwBytesWritten = 0;
     DWORD dwAttrBytesWritten = 0;
-	
-	SMALL_RECT smallRect = {0,0,(short)(nScreenWidth-1), (short)(nScreenHeight-1)};
+    SMALL_RECT m_rectWindow = { 0, 0, 1, 1 };
+    COORD coord = { (short)nScreenWidth, (short)nScreenHeight };
 
-	SetConsoleWindowInfo(hConsole ,1 , &smallRect);
-	SetConsoleScreenBufferSize(hConsole ,{(short)nScreenWidth,(short)nScreenHeight});
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 
+
+    SetConsoleWindowInfo(hConsole, TRUE, &m_rectWindow);
+
+    SetConsoleScreenBufferSize(hConsole ,coord);
+    SetConsoleActiveScreenBuffer(hConsole);
     
-    char cScore[50]="";
-    string sTitle = "Snake game ----- Score : ";
+    m_rectWindow = {0,0,(short)(nScreenWidth-1), (short)(nScreenHeight-1)};
+    SetConsoleWindowInfo(hConsole ,1 , &m_rectWindow);
 
-    unsigned int TIME_SLEEP = 100;
-
-
+    CHAR_INFO* m_bufScreen = new CHAR_INFO[nScreenWidth*nScreenHeight];
+    memset(m_bufScreen, 0, sizeof(CHAR_INFO) * nScreenWidth * nScreenHeight);
 
     while(!isDead){
 
         sprintf(cScore,"%.1f",fScore);
         SetConsoleTitle((sTitle+cScore).c_str());
-	
+
         //printf(&screen[1 + nScreenWidth],"Score : %d" , nScore );
 
         Sleep(TIME_SLEEP);
@@ -138,10 +156,10 @@ int main(){
         }
 
         // snake wall collision
-        if(snake.front().x < 0 || snake.front().x >= nScreenWidth){
+        if(snake.front().x < 0 || snake.front().x > nScreenWidth){
             isDead = true;
         }
-        if(snake.front().y <= 0 || snake.front().y >= nScreenHeight){
+        if(snake.front().y < 0 || snake.front().y > nScreenHeight){
             isDead = true;
         }
 
